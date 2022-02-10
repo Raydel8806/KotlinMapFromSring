db2679f56a158c7c70586ef5755a0d0f5e77f75b
Adnan Fuentes <raydel8806@gmail.com>
2022-02-10 05:41:40 -0800

    first commit

---
+++ app/src/main/java/com/campingstudio/kotlinmapfromsring/Main.kt
@@ -0,0 +1,16 @@
+
+fun main() {
+    var mapChar = mapOf("a" to "A","c" to "C","b" to "B","a" to "A")
+
+    var strMap = mapChar.toString()
+    var strMap2 = ""
+
+    for (i in 1 .. strMap.toCharArray().size-2) strMap2 += strMap.toCharArray()[i]
+
+    var mapFinal = strMap2.split(",").associate {
+        val (left, right) = it.split("=")
+        left to right
+    }
+
+    println(mapFinal["a"])
+}
